0dcdb7005bff409edc0848ab80ab01f7ee7c6f3e
lubgade <lubgade@yahoo.com>
2018-01-31 09:45:53 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
+
     // socket.emit('newMessage', {
     //     from: 'Mike',
     //     text: "hello",
